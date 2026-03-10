4e42a77389155efa11d618c13f587e9f1283f551
GauravSirari <gauravsirari424@gmail.com>
2026-03-10 15:09:14 +0530

    add product.html on view

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req,res)=>{
+    return res.send('Welcome to Inventory App');
+});
+
+server.post('/', (req,res)=>{
+    return res.send('Welcome to Inventory App');
+});
+
+server.listen(3400);
