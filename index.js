3e34f11d5dc35b824c74785378c782a25ab5531f
GauravSirari <gauravsirari424@gmail.com>
2026-03-17 16:11:08 +0530

    create model and controller

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+import express from 'express'
+import ProductController from './src/controllers/product.controller.js';
+// const express = require('express');
 
 const server = express();
 
-server.get('/', (req,res)=>{
-    return res.send('Welcome to Inventory App');
-});
-
-server.post('/', (req,res)=>{
-    return res.send('Welcome to Inventory App');
-});
-
+// create an instance of ProductController
+const productController = new ProductController(); 
+server.get('/', (productController.getProducts));
+server.use(express.static('src/views'));
+    // return res.send('Welcome to Inventory App');
 server.listen(3400);
+console.log('Server is listening on pert 3400');
